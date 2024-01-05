7c607625007d02133a62e7c32b25a745bed41a45
Mandy M. <127141046+mandaeliza@users.noreply.github.com>
2024-01-05 13:18:39 +0100

    Added eventListener and created li

--- js/script.js
+++ js/script.js
@@ -10,3 +10,13 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+
+addGuestButton.addEventListener("click", function() {
+    const guest = guestInput.value;
+    // console.log(guest);
+    if  (guest !== "") {
+        const listItem = document.createElement("li");
+        listItem.innerText = guest;
+        guestList.append(listItem);
+    }
+  })
