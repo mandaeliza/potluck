9d595232ec02c5b4270466b70afe7c330c601f9a
Mandy M. <127141046+mandaeliza@users.noreply.github.com>
2024-01-05 15:45:06 +0100

    Add eventListener and update potluck array

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ const assignButton = document.querySelector(".assign");
 // items to assign
 const assignedItems = document.querySelector(".assigned-items")
 
-
 addGuestButton.addEventListener("click", function() {
     const guest = guestInput.value;
     // console.log(guest);
@@ -23,18 +22,17 @@ addGuestButton.addEventListener("click", function() {
     addToList(guest);
     updateGuestCount();
     clearInput();
-} 
-    
+}    
 });
-
-const clearInput = function() {
-        guestInput.value = "";
-};
 
 const addToList = function (guest) {
     const listItem = document.createElement("li");
     listItem.innerText = guest;
     guestList.append(listItem);   
+};
+
+const clearInput = function() {
+        guestInput.value = "";
 };
 
 const updateGuestCount = function () {
@@ -50,20 +48,37 @@ const updateGuestCount = function () {
 };
 
 const assignItems = function () {
-  let potluckItems = ["Peach and Prosciutto Bruschetta", "Veggie Nori Rolls", 
-  "Tomato & Mozzarella Caprese Skewers", "Stuffed Peppadews with Parmesan & Salami", 
-  "3-Cheese Tomato Tart", "Lemon Cream Icebox Cake", "Pesto Pasta Salad", 
-  "Tangy Cucumber Salad", "White Gazpacho", "Mini Ice Cream Cakes", "Ice", "Plates and Cutlery"];
+const potluckItems = [
+    "cheese",
+    "crackers",
+    "fresh fruit",
+    "coleslaw",
+    "apple cake",
+    "gazpacho",
+    "baguette",
+    "egg salad",
+    "potato salad",
+    "summer rolls",
+    "hummus",
+    "cookies"
+  ]; 
+  
+  const allGuests = document.querySelectorAll(".guest-list li");
+
+  for (let guest of allGuests) {
+    let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+    let randomPotluckItem = potluckItems[randomPotluckIndex];
+
+    let listItem = document.createElement("li");
+    listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+    assignedItems.append(listItem); 
+
+    potluckItems.splice(randomPotluckIndex, 1); 
+  }
+    
 };
 
-const allGuests = document.querySelectorAll(".guest-list li");
-for (let guest of allGuests) {
-  const randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
-  const randomPotluckItem = putluckItems[randomPotluckIndex];
-  const listItem = document.createElement("li");
-  listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
-  assignItems.append(listItem); 
-};
-
-
-
+assignButton.addEventListener("click", function() {
+  assignItems();
+  assignButton.disabled = true;
+});
